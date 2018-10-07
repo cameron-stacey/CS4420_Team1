231d83052a1fcc020323e45a795b53944580399d
Jonathan DeCock <jdecock@uccs.edu>
2018-10-07 23:25:32 +0000

    user tables

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get ('register', function () {
+    return view('register');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
